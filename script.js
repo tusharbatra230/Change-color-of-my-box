8b34c9f3d7591c7b6b9b6f043d7d785a715713b9
tusharbatra230 <89705175+tusharbatra230@users.noreply.github.com>
2021-08-30 20:50:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// document.querySelector(".Aman").addEventListener('click', function()
+// {
+// document.querySelector(".para").innerHTML="The Poetry Translation Centre is very proud to partner with the Aké Arts and Books Festival to present a special series of translation workshops on the Nigerian languages of Yoruba, Igbo and Hausa"
+// }
+// )
+
+
+document.getElementById("Box-1").addEventListener('click', function()
+{
+
+     document.querySelector(".BOX").style.backgroundColor="darkred";
+ })
+
+ document.getElementById("Box-2").addEventListener('click', function()
+{
+
+     document.querySelector(".BOX").style.backgroundColor="gold";
+ })
+ document.getElementById("Box-3").addEventListener('click', function()
+{
+
+     document.querySelector(".BOX").style.backgroundColor="indigo";
+ })
+ document.getElementById("Box-4").addEventListener('click', function()
+{
+
+     document.querySelector(".BOX").style.backgroundColor="lightcoral";
+ })
+
+ document.getElementById("Box-5").addEventListener('click', function()
+ {
+ 
+      document.querySelector(".BOX").style.backgroundColor="white";
+  })
